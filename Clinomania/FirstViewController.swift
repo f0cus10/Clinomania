2eab1ca0d748f11e6c6325778b096060f2961e67
f0cus10 <iakhan4425@gmail.com>
2020-05-08 17:07:04 -0400

    Initial Commit

---
+++ Clinomania/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Clinomania
+//
+//  Created by Iftikhar A. Khan on 5/8/20.
+//  Copyright © 2020 Iftikhar A. Khan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Clinomania/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Clinomania
+//
+//  Created by Iftikhar A. Khan on 5/8/20.
+//  Copyright © 2020 Iftikhar A. Khan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
