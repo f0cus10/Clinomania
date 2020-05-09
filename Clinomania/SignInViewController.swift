04d00dbed6d39f0379a077ab1f3c5a881711fe42
f0cus10 <iakhan4425@gmail.com>
2020-05-09 01:20:44 -0400

    SignInViewController (User Interface) - disable keyboard on touching elsewhere

--- Clinomania/SignInViewController.swift
+++ Clinomania/SignInViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class SignInViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signInButton: UIButton!
     
     override func viewWillAppear(_ animated: Bool){
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = true
+        
+        // hide keyboard
+        let keyboardDismissRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        self.view.addGestureRecognizer(keyboardDismissRecognizer)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         signInButton.layer.cornerRadius = 5
+    }
+    
+    // MARK: - Helper functions
+    @objc func hideKeyboard(_ gestureRecognizer: UIGestureRecognizer){
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return
     }
 }
