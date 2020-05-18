b7f8c9dd09a5391e39b9e9eeba9c56f1d6420492
f0cus10 <iakhan4425@gmail.com>
2020-05-18 16:40:43 -0400

    Add custom table view cell for the second tab

---
+++ Clinomania/JobSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobSearchTableViewCell.swift
+//  Clinomania
+//
+//  Created by Iftikhar A. Khan on 5/18/20.
+//  Copyright © 2020 Iftikhar A. Khan. All rights reserved.
+//
+
+import UIKit
+
+class JobSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
